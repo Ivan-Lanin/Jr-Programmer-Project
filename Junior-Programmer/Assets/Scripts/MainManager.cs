cacaf203da8187c7b2549cba708367d55ccc9986
Ivan-Lanin <ivanandrlanin@gmail.com>
2023-06-28 23:53:29 +0200

    Data persistence between sessions

--- Junior-Programmer/Assets/Scripts/MainManager.cs
+++ Junior-Programmer/Assets/Scripts/MainManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -18,5 +19,21 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
     }
 }
